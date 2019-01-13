5920f2842702b53f6e8d8ae08d9bdd49c3220675
Bogdan CIOBANU <bciobanu@bbu.bitdefender.biz>
2019-01-13 20:18:51 +0200

    Add idle read test

---
+++ tests/idle_read.py
@@ -0,0 +1,19 @@
+import sys
+import time
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: python3 idle_read.py [path_to_file] [chunk_size] [timeout]")
+        sys.exit(-1)
+
+    with open(sys.argv[1]) as f:
+        while True:
+            data = f.read(int(sys.argv[2]))
+            print("[idle_read]", data)
+            time.sleep(float(sys.argv[3]))
+            if not data:
+                break
+
+
+if __name__ == '__main__':
+    main()
